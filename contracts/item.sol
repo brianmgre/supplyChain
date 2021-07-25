84fbeb6c5d96a0e742d621a8083f1f341545b70f
Brian G <brian.gregorius@gmail.com>
2021-07-25 15:33:26 -0400

    finished

---
+++ contracts/item.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.8.5;
+
+import "./ItemManager.sol";
+
+contract Item {
+    uint256 public priceInWei;
+    uint256 public index;
+    uint256 public paidWei;
+
+    ItemManager parentContract;
+
+    constructor(
+        ItemManager _parentContract,
+        uint256 _priceInWei,
+        uint256 _index
+    ) {
+        priceInWei = _priceInWei;
+        index = _index;
+        parentContract = _parentContract;
+    }
+
+    receive() external payable {
+        require(msg.value == priceInWei, "We don't support partial payments");
+        require(paidWei == 0, "Item is already paid!");
+        paidWei += msg.value;
+        (bool success, ) =
+            address(parentContract).call{value: msg.value}(
+                abi.encodeWithSignature("triggerPayment(uint256)", index)
+            );
+        require(success, "Delivery did not work");
+    }
+
+    fallback() external {}
+}
---
+++ test/testItemManager.js
@@ -0,0 +1,19 @@
+const ItemManager = artifacts.require("./ItemManager.sol");
+
+contract("ItemManager", (accounts) => {
+	it("...should be able to add an Item.", async () => {
+		const manager = await ItemManager.deployed();
+
+		const itemName = "Test 1";
+		const itemPrice = 500;
+
+		const result = await manager.createItem(itemName, itemPrice, {
+			from: accounts[0],
+		});
+
+		assert.equal(result.logs[0].args._itemIndex, 0, "It's not the first item");
+
+		const item = await manager.items(0);
+		assert.equal(item._identifier, itemName, "The identifier was different");
+	});
+});
